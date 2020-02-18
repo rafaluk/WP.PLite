4566a459fafb38722eb2a526e55aab8e8a2908ad
rafalklat <rafalklat@quantexa.com>
2020-02-18 20:59:55 +0100

    pass target link and show it on art site

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, redirect, url_for, flash, session
 import app.data.data_resolver as data
+from flask import request
+
 
 @app.route('/')
 def nothing():
@@ -10,8 +12,16 @@ def nothing():
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     links = data.wp()
-    print(links)
-    fiut = "dupszczi"
+    # print(links)
+
     return render_template('index.html', title='Start',
                            links=links)
 
+
+@app.route('/art', methods=['GET', 'POST'])
+def art():
+    link = request.args.get('link')
+    print(link)
+    
+
+    return render_template('art.html', link=link)
